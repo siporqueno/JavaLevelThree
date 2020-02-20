8549ccd838ca7f74ce2a2a0ca74d73916a4a0cd5
siporqueno <strike1000001@gmail.com>
2020-02-20 09:56:45 +0300

    Lesson 7, task 2 (reflection)
    Small refactoring here and there

--- src/main/java/lesson_7/refl/HomeWorkChecker.java
+++ src/main/java/lesson_7/refl/HomeWorkChecker.java
@@ -48,33 +48,33 @@ public class HomeWorkChecker {
             Constructor constructor = cHw.getConstructor();
             Object hw = constructor.newInstance();
 
-            // call of method calculate for int arguments
+            // call of method "calculate" for int arguments
             Method calculateInt = cHw.getDeclaredMethod("calculate", int.class, int.class, int.class, int.class);
             calculateInt.setAccessible(true);
             System.out.println("Call of method calculate for int, using formula a*(b+(c/d)) and params 2, 2, 6, 3 returns " + calculateInt.invoke(hw, 2, 2, 6, 3) + "\n");
 
-            // call of method calculate for float arguments
+            // call of method "calculate" for float arguments
             Method calculateFloat = cHw.getDeclaredMethod("calculate", float.class, float.class, float.class, float.class);
             calculateFloat.setAccessible(true);
             System.out.println("Call of method calculate for float, using formula a*(b+(c/d)) and params 1.5f, 0.5f, 3f, 2f returns " + calculateFloat.invoke(hw, 1.5f, 0.5f, 3f, 2f) + "\n");
 
-            // call of method checkTwoNumbers
+            // call of method "checkTwoNumbers"
             Method checkTwoNumbers = cHw.getDeclaredMethod("checkTwoNumbers", int.class, int.class);
             checkTwoNumbers.setAccessible(true);
             System.out.println("Call of method checkTwoNumbers to check if their sum is within [10, 20] for 5 and 10 returns " + checkTwoNumbers.invoke(hw, 5, 10) + "\n");
 
-            // call of method void printIsPositive [int]
+            // call of method "printIsPositive"
             Method printIsPositive = cHw.getDeclaredMethod("printIsPositive", int.class);
             printIsPositive.setAccessible(true);
             System.out.print("Call of method printIsPositive to print positive/negative if the given number is positive/negative prints the following for -5: ");
             printIsPositive.invoke(hw, -5);
 
-            // call of method isNegative
+            // call of method "isNegative"
             Method isNegative = cHw.getDeclaredMethod("isNegative", int.class);
             isNegative.setAccessible(true);
             System.out.println("\nCall of method isNegative to return true if the given number is negative returns the following for -5: " + isNegative.invoke(hw, -5) + "\n");
 
-            // call of method void printWelocome [class java.lang.String]
+            // call of method "printWelocome"
             Method printWelocome = cHw.getDeclaredMethod("printWelocome", String.class);
             printWelocome.setAccessible(true);
             System.out.print("Call of method printWelocome to print Welcome name, where name - parameter prints the following for the parameter Astra: ");
@@ -87,12 +87,12 @@ public class HomeWorkChecker {
 
             // call of method "main"
             Method main = cHw.getDeclaredMethod("main", String[].class);
-            System.out.println("Call of method main");
+            System.out.println("Call of method main:");
             Thread.sleep(1000);
             main.invoke(hw, new Object[]{new String[]{}});
 
         } catch (ClassNotFoundException | MalformedURLException | InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException | InterruptedException e) {
-            System.out.println("Something went wrong in the method checkOneHomeWorkFile");
+            System.out.println("Something went wrong inside the method checkOneHomeWorkFile:");
             e.printStackTrace();
         }
     }
